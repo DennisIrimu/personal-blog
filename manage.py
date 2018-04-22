932c6e07f88192f0c7cc60a0bd60f98377dae2aa
dnyt <lyricaldnyt@gmail.com>
2018-04-22 14:38:21 +0300

    Initialize shell

--- manage.py
+++ manage.py
@@ -1,13 +1,15 @@
-
 from flask_script import Manager,Server
 from app import create_app,db
+
+from app.models import User
 
 app = create_app('default')
 
 manager = Manager(app)
-
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
 manager.add_command('server', Server)
 
 if __name__ == '__main__':
     manager.run()
-
