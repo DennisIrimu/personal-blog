b0264ec95e521d1d1bed724152dbb34c9014b374
dnyt <lyricaldnyt@gmail.com>
2018-04-22 20:55:50 +0300

    Saving and committting to database

--- app/models.py
+++ app/models.py
@@ -26,6 +26,16 @@ class User(UserMixin,db.Model):
     posts = db.relationship('Post',backref='user', lazy='dynamic')
     comments = db.relationship('Comment', backref='user', lazy='dynamic')
 
+    @property
+    def password(self):
+        raise AtributeError('Password Attribute error')
+
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
         return f'User {self.username}'
