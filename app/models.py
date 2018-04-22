e382f871bc14ff8216bc9f8a085e1bc8899a660b
dnyt <lyricaldnyt@gmail.com>
2018-04-22 14:53:18 +0300

    Update user class

--- app/models.py
+++ app/models.py
@@ -4,6 +4,10 @@ class User(db.Model):
     __tablename__= 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(244))
+    email = db.Column(db.String(50), unique=True, index=True)
+    password_hash = db.Column(db.String(20))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
 
     def __repr__(self):
         return f'User {self.username}'
--- manage.py
+++ manage.py
@@ -1,6 +1,5 @@
-from flask_script import Manager,Server
 from app import create_app,db
-
+from flask_script import Manager,Server
 from app.models import User
 
 app = create_app('default')
