d3a0651c239302dc1da52b387b4841454ebd350a
dnyt <lyricaldnyt@gmail.com>
2018-04-23 09:58:09 +0300

    Validate user and email

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,3 +8,18 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[Required()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Input email address', validators=[Required(),Email()])
+    username = StringField('Input user name', validators=[Required()])
+    password = PasswordField('Password',validators=[Required()])
+    password_confirm = PasswordField('Confirm Passwords', validators=[Required(),EqualTo('password_confirm',message="Passwords must match")])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError('Email Address is already in use')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username=data_field.data).first():
+            raise ValidationError('Username already in use')
