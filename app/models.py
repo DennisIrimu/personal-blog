30141336f6299c91dc16261f5c15b7c0b2d3b416
dnyt <lyricaldnyt@gmail.com>
2018-04-23 12:07:42 +0300

    Create function to save posts

--- app/models.py
+++ app/models.py
@@ -62,4 +62,6 @@ class Post(db.Model):
     user_id=db.Column(db.Integer, db.ForeignKey("users.id"))
     comments = db.relationship('Comment', backref='post', lazy='dynamic', )
 
-    
+    def save_post(self):
+        db.session.add(self)
+        db.session.commit()
