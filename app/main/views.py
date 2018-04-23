53ba9972a2ac00b72b45f7c745502ba0418cfddf
dnyt <lyricaldnyt@gmail.com>
2018-04-23 10:50:17 +0300

    Create main route

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,15 @@
+from flask import render_template,request,redirect,url_for,abort,flash
 from . import main
+from ..models import User,Role,Post
+from .forms import CommentForm,PostForm
+from . import main
+from flask_login import login_required
+from .. import db
+
 
 @main.route('/')
 def index():
-    return '<h1> Hello World </h1>'
+    title = 'Home'
+    posts = Post.get_posts()
 
+    return render_template('index.html', title = title, posts=posts)
