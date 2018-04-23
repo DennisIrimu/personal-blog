311148e71f113a80364bcfb232ba0f8a786ae5f2
dnyt <lyricaldnyt@gmail.com>
2018-04-23 11:42:41 +0300

    Test the role class

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app(config_state):
     app = Flask(__name__)
@@ -15,6 +17,8 @@ def create_app(config_state):
 
     bootstrap.init_app(app)
     db.init_app(app)
+
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
